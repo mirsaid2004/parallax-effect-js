3d9fd75593f8076a8ea644fa3aec9f48ce711dd3
Mirakhmedov Mirsaid <66273717+mirsaid-number1@users.noreply.github.com>
2024-05-28 02:20:04 +0500

    debug: compatibility

---
+++ babel.config.cjs
@@ -0,0 +1,11 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+  presets: [
+    [
+      "@babel/preset-env",
+      {
+        targets: "> 0.25%, not dead",
+      },
+    ],
+  ],
+};
--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
+import "core-js/stable";
+import "regenerator-runtime/runtime";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
